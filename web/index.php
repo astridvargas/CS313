34175b45ec1d1b6a13daf5cb78c863ebb5e7d0e9
astridvargas <var16004@byui.edu>
2019-09-28 22:28:30 -0600

    More changes!

--- web/index.php
+++ web/index.php
@@ -1,5 +1,70 @@
-<?php
+<!DOCTYPE html>
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <!-- Website Title -->
 
-  phpinfo();
+        <title>Astrid's Homepage</title>
 
-?>
+        <!-- Meta Link -->
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- CSS File -->
+
+        <link rel="stylesheet" href="assignment02.css">
+
+        <!-- Google Fonts -->
+
+        <link href="https://fonts.googleapis.com/css?family=Cousine|Oswald&display=swap" rel="stylesheet">
+
+    </head>
+    <body>
+
+        <!-- Go to Top Button! -->
+
+        <!-- <button onclick="topFunction()" id="topButton" title="Go to top">Top</button> -->
+        
+        <!-- Welcome Picture -->
+
+        <div class="container">
+            <img id="mainPicture" src="us.jpg" alt="Us">
+            <div class="content">
+                <h1>Welcome to Astrid Vargas's Website</h1>
+            </div>
+        </div>
+
+        <!-- Introduction -->
+
+        <h1>Student // Developer // Sushi Lover // Wife</h1>
+
+        <!-- Grids -->
+
+        <div class="gridContainer">
+            <div>
+                <h3>A little more about me</h3>
+                <a href="../web/Week02/assignment02.1.html">
+                    <img class="smallPictures" src="more.jpg" alt="About">
+                </a>
+            </div>
+            
+            <div>
+                
+            </div>
+
+            <div>
+                <h3>Assignments</h3>
+                <a href="../web/Week02/assignment02.2.html">
+                        <img class="smallPictures" src="https://images.unsplash.com/photo-1551033406-611cf9a28f67?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="About">
+                </a>
+            </div>
+        </div>
+
+        <!-- PHP -->
+
+        <br><br>
+<!-- 
+        <?php
+        echo "<p>Copyright &copy; 2019" . " Astrid Vargas</p>";
+        ?> -->
+    </body>
+</html>
