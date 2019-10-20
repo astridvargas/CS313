6784c503ca8bd90df02b0f2a13ddaad143e53a52
astridvargas <var16004@byui.edu>
2019-10-19 19:43:43 -0600

    New

---
+++ web/Project/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Astrid Vargas's Project</title>
+        <link rel="stylesheet" href="project.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    </head>
+    <body>
+
+    <h1>Contacts</h1>
+        
+    
+    <script src="" async defer></script>
+    </body>
+</html>
