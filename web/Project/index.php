4718fd1d1dfab20d3edd9b0be2cfe85a88282856
astridvargas <var16004@byui.edu>
2019-10-19 22:47:27 -0600

    K

--- web/Project/index.php
+++ web/Project/index.php
@@ -21,10 +21,12 @@ $db = get_db();
         
             while ($row = $statement->fetch(PDO::FETCH_ASSOC))
             {
-	            $user = $row['users_name'];
+                $user = $row['users_name'];
+                $telephone = $row['user_telephone'];
 	            // $userTelephone_id = $row['userTelephone_id'];
 	            // $userAddress_id = $row['userAddress_id'];
                 echo "<p><strong> $user </strong><p>";
+                echo "<p><strong> $telephone </strong><p>";
                 // echo "<p><strong>$userName_id - $userTelephone_id - $userAddress_id </strong><p>";
             }
         ?>
@@ -33,23 +35,4 @@ $db = get_db();
     </div>
 
 </body>
-</html>
-
-<!DOCTYPE html>
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
-    </head>
-    <body>
-        <!--[if lt IE 7]>
-            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
-        
-        <script src="" async defer></script>
-    </body>
 </html>
