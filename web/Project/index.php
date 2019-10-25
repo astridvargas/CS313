7eec815078ce39cfdb1b2193fcc6be1c97ab85bf
astridvargas <var16004@byui.edu>
2019-10-25 12:44:39 -0600

    Testing

--- web/Project/index.php
+++ web/Project/index.php
@@ -129,7 +129,7 @@ $db = get_db();
         </div>
         
 
-        <!-- <php
+        <?php
 
             $statement = $db->prepare("SELECT users_name FROM userName");
             $statement->execute();
@@ -144,7 +144,7 @@ $db = get_db();
                 echo "<p><strong> $telephone </strong><p>";
                 echo "<p><strong>$userName_id - $userTelephone_id - $userAddress_id </strong><p>";
             }
-        ?> -->
+        ?>
 
 
     </div>
