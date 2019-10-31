e4a3b987208d47e6d507d9f18f3cdaab445dafbb
astridvargas <var16004@byui.edu>
2019-10-31 15:58:00 -0600

    Fixing

--- web/Project/index.php
+++ web/Project/index.php
@@ -52,82 +52,6 @@ $db = get_db();
         </div>
 
         <br>
-
-        <div class="container">
-            <div class="row">
-                <div class="col s8 offset-s2">
-                    <h3 class="center">List of Contacts</h3>
-
-                    <input type="text" id="search" placeholder="Search Contacts">
-                    
-                    <div class="center">
-                        <p class="total">Total: <span>2</span> Contacts</p>
-                    </div>
-                    
-                    
-                    <div class="tableContainer">
-                        <table class="highlight">
-                            <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Phone Number</th>
-                                    <th>Address</th>
-                                    <th>Actions</th>
-                                </tr>
-                            </thead>
-
-                            <tbody>
-                                <tr>
-                                    <td>Astrid Vargas</td>
-                                    <td>385-333-6428</td>
-                                    <td>111 Fake Address Rexburg, Idaho 83440</td>
-                                    <td>
-                                        <a class= "editButton" href="editContact.php">
-                                            <i class="small material-icons">edit</i>
-                                        </a>
-
-                                        <button type="button" class="deleteButton">
-                                            <i class="small material-icons">delete</i>
-                                        </button>
-                                    </td>
-                                </tr>
-
-                                <tr>
-                                    <td>Chris Lujo</td>
-                                    <td>650-826-7438</td>
-                                    <td>111 Fake Address Rexburg, Idaho 83440</td>
-                                    <td>
-                                        <a class= "editButton" href="editContact.php">
-                                            <i class="small material-icons">edit</i>
-                                        </a>
-
-                                        <button type="button" class="deleteButton">
-                                            <i class="small material-icons">delete</i>
-                                        </button>
-                                    </td>
-                                </tr>
-
-                                <tr>
-                                    <td>Kenneth Vargas</td>
-                                    <td>208-678-3134</td>
-                                    <td>111 Fake Address Rexburg, Idaho 83440</td>
-                                    <td>
-                                        <a class= "editButton" href="editContact.php">
-                                            <i class="small material-icons">edit</i>
-                                        </a>
-
-                                        <button type="button" class="deleteButton">
-                                            <i class="small material-icons">delete</i>
-                                        </button>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    
-                </div>
-            </div>
-        </div>
         
         <div class="container">
             <div class="row">
@@ -162,9 +86,6 @@ $db = get_db();
                             $name = $row['_name'];
                             $telephone = $row['_telephone'];
                             $address = $row['_address'];
-                            // $userAddress_id = $row['userAddress_id'];
-                            // echo "<p><strong> $user </strong><p>";
-                            // echo "<p><strong> $telephone </strong><p>";
 
                             echo "<tbody>
                                 <tr>
@@ -183,7 +104,6 @@ $db = get_db();
 
                                 </td></tr>
                             ';
-                            // echo "<p><strong>$name - $telephone - $address </strong><p>";
                         }
 
                         echo '</tbody></table>';
